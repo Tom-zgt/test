a41b1ff07b20a9b892ac136c60ea6c35c53e2301
zgt <zhengguangtingzgt@icloud.com>
2020-10-12 08:55:40 +0800

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by 郑光庭 on 2020/10/12.
+//  Copyright © 2020 郑光庭. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
